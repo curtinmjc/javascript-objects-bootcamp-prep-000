0f25c31599f7e40d69d8f78a823b5c8ba5dc2ff1
Mike Curtin <curtinmjc@gmail.com>
2018-09-18 20:14:53 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 var playlist = {Beatles: 'Love me do', 'Paul Simon': 'Graceland'};
 
 function updatePlaylist(obj, artist, song) {
-  return Object.assign({}, obj, {artist: song});
+//  return Object.assign({}, obj, {artist: song});
+  obj.artist = song;
+  return obj;
 }
 
 function removeFromPlaylist(obj, artist) {
